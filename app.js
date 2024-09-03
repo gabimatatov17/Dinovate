b87c0a6bdb6807c0aabb30e686702f4003dd64ab
noamgloberman1 <noamg.j2@gmail.com@gmail.com>
2024-09-03 21:06:44 +0300

    working express

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));   
