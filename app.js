ae52fe580efb7321cb5a903fb473e704cf4cca59
noamgloberman1 <noamg.j2@gmail.com@gmail.com>
2024-09-03 21:45:03 +0300

    added backend for home page

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
+// routes
+const home = require('./routes/home');
+
+// express settings
 app.set('views', path.join(__dirname, 'views'));   
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// routes
+app.use("/", home);
 
 // Start the server
 const port = 3000;
