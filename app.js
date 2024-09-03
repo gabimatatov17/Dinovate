f6117a598ebfe425d12ee7e6af967d1072243437
noamgloberman1 <noamg.j2@gmail.com@gmail.com>
2024-09-03 20:41:00 +0300

    project setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));   
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
