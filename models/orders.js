4b8e3af12addc656b7440ba090b8bf593a035ea6
gabimatatov <gavriel.matatov@veeva.com>
2024-09-05 21:08:03 +0300

    added models and sample data

---
+++ models/orders.js
@@ -0,0 +1,96 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    orderId: {
+        type: Number,
+        required: true
+    },
+    customerId: {
+        type: Number,
+        required: true
+    },
+    cards: {
+        type: Array,
+        required: true
+    },
+    totalPrice: {
+        type: Number,
+        required: true
+    },
+    shippingAdress: {
+        type: String,
+        required: true
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now 
+    }
+});
+
+const Order = mongoose.model("Order", orderSchema, "orders");
+module.exports = Order;
+
+// Example of cards array type
+// cards = [
+//     {
+//         cardId: cardId,
+//         greeting: "Happy Birthday"
+//     }
+// ]
+
+
+
+
+
+////
+// mongoose.connect('mongodb://localhost:27017/Dinovate')
+//     .then(() => console.log('Connected to MongoDB!'))
+//     .catch(err => console.error('Could not connect to MongoDB...', err));
+
+// const orders = [
+//     {
+//         orderId: 1,
+//         customerId: 1,
+//         cards: [
+//             {
+//                 cardId: 1,
+//                 greeting: "Happy Birthday Victoria!"
+//             }
+//         ],
+//         totalPrice: 19.90,
+//         shippingAdress: "45 Rothschild Boulevard, Tel Aviv-Yafo, 65784, Israel"
+//     },
+//     {
+//         orderId: 2,
+//         customerId: 2,
+//         cards: [
+//             {
+//                 cardId: 2,
+//                 greeting: "Get Well Soon Jude!"
+//             }
+//         ],
+//         totalPrice: 19.90,
+//         shippingAdress: "123 Baker Street, London, NW1 6XE, United Kingdom"
+//     },
+//     {
+//         orderId: 3,
+//         customerId: 2,
+//         cards: [
+//             {
+//                 cardId: 1,
+//                 greeting: "Happy Birthday Megan!"
+//             },
+//             {
+//                 cardId: 2,
+//                 greeting: "Get Well Soon David!"
+//             }
+//         ],
+//         totalPrice: 39.80,
+//         shippingAdress: "123 Baker Street, London, NW1 6XE, United Kingdom"
+//     }
+// ];
+
+// Order.insertMany(orders)
+//     .then(() => console.log('All orders saved successfully!'))
+//     .catch(err => console.error('Error saving orders:', err.message))
+//     .finally(() => mongoose.connection.close());
