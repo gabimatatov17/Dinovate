db8167ce4f93654e68e56bbbd6dd4e333dd7d51f
gabimatatov <gavriel.matatov@veeva.com>
2024-09-17 21:22:11 +0300

    removed admin login option

--- models/customers.js
+++ models/customers.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const customerSchema = new mongoose.Schema({
-    customerId: {
+    id: {
         type: Number,
         required: true
     },
@@ -40,7 +40,12 @@ const customerSchema = new mongoose.Schema({
     },
     orders: {
         type: Array     // Array of orderId's or Order Schema
-    }
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+        required: true
+    },
 });
 
 const Customer = mongoose.model("Customer", customerSchema, "customers");
@@ -56,20 +61,70 @@ module.exports = Customer;
 
 // const customers = [
 //     {
-//         customerId: 1,
+//         id: 1,
+//         firstName: 'Gabi',
+//         lastName: 'Matatov',
+//         email: ' gabi.matatov@dinovate.com ',
+//         gender: 'Male',
+//         birthDate: new Date('2000-01-01'),
+//         password: 'password123',
+//         isAdmin: true
+//     },
+//     {
+//         id: 2,
+//         firstName: 'Din',
+//         lastName: 'Nehmadi',
+//         email: ' Din.Nehmadi@dinovate.com ',
+//         gender: 'Male',
+//         birthDate: new Date('2000-01-01'),
+//         password: 'password123',
+//         isAdmin: true
+//     },
+//     {
+//         id: 3,
+//         firstName: 'Noam',
+//         lastName: 'Globerman',
+//         email: ' Noam.Globerman@dinovate.com ',
+//         gender: 'Male',
+//         birthDate: new Date('2000-01-01'),
+//         password: 'password123',
+//         isAdmin: true
+//     },
+//     {
+//         id: 4,
+//         firstName: 'Mia',
+//         lastName: 'Wolfson',
+//         email: ' Mia.Wolfson@dinovate.com ',
+//         gender: 'Female',
+//         birthDate: new Date('2000-01-01'),
+//         password: 'password123',
+//         isAdmin: true
+//     },
+//     {
+//         id: 5,
+//         firstName: 'Yonatan',
+//         lastName: 'Sini',
+//         email: ' Yonatan.Sini@dinovate.com ',
+//         gender: 'Male',
+//         birthDate: new Date('2000-01-01'),
+//         password: 'password123',
+//         isAdmin: true
+//     },
+//     {
+//         id: 6,
 //         firstName: 'David',
 //         lastName: 'Beckham',
-//         email: ' David.Beckham@example.com ',
+//         email: ' David.Beckham@dinovate.com ',
 //         gender: 'Male',
 //         birthDate: new Date('2000-01-01'),
 //         password: 'password123',
 //         orders : [1]
 //     },
 //     {
-//         customerId: 2,
+//         id: 7,
 //         firstName: 'Phil',
 //         lastName: 'Foden',
-//         email: ' Phil.Foden@example.com ',
+//         email: ' Phil.Foden@dinovate.com ',
 //         gender: 'Male',
 //         birthDate: new Date('2000-01-01'),
 //         password: 'password123',
