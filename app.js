1d1dfad93dc1e414ab31046110d6e94cc9bae0f8
gabimatatov <gavriel.matatov@veeva.com>
2024-10-01 01:26:50 +0300

    added session manager

--- app.js
+++ app.js
@@ -38,49 +38,31 @@ app.use(session({
 
 // !!delete this bit before pulling from main
 // Simulate session cart data and customer by fetching from MongoDB 
-app.use(async (req, res, next) => {
-    // Simulate cart data
-    if (!req.session.cart) {
-        try {
-            // Fetch two random cards from the database
-            const cards = await Card.aggregate([{ $sample: { size: 2 } }]);
+// app.use(async (req, res, next) => {
+//     // Simulate cart data
+//     if (!req.session.cart) {
+//         try {
+//             // Fetch two random cards from the database
+//             const cards = await Card.aggregate([{ $sample: { size: 2 } }]);
 
-            if (cards.length >= 2) {
-                req.session.cart = cards.map(card => ({
-                    cardId: card.cardId,
-                    cardName: card.cardName,
-                    price: card.price,
-                    quantity: 1,  // Default quantity
-                    image: card.image_location
-                }));
-                console.log('Simulated cart data added to session');
-            } else {
-                console.log('Not enough cards in the database to simulate the cart');
-            }
-        } catch (error) {
-            console.error('Error fetching cards from MongoDB:', error.message);
-        }
-    }
-
-    // Simulate customer data
-    if (!req.session.customer) {
-        try {
-            // Fetch one random customer from the database
-            const customer = await Customer.aggregate([{ $sample: { size: 1 } }]);
-
-            if (customer.length > 0) {
-                req.session.customer = customer[0];  // Store the entire customer object in session
-                console.log('Simulated customer data added to session');
-            } else {
-                console.log('No customers found in the database');
-            }
-        } catch (error) {
-            console.error('Error fetching customer from MongoDB:', error.message);
-        }
-    }
-
-    next();
-});
+//             if (cards.length >= 2) {
+//                 req.session.cart = cards.map(card => ({
+//                     cardId: card.cardId,
+//                     cardName: card.cardName,
+//                     price: card.price,
+//                     quantity: 1,  // Default quantity
+//                     image: card.image_location
+//                 }));
+//                 console.log('Simulated cart data added to session');
+//             } else {
+//                 console.log('Not enough cards in the database to simulate the cart');
+//             }
+//         } catch (error) {
+//             console.error('Error fetching cards from MongoDB:', error.message);
+//         }
+//     }
+//     next();
+// });
 
 
 
