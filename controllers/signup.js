26ddddcd6d38aaaab8a288a1f3ce7cbeccb64a05
gabimatatov <gavriel.matatov@veeva.com>
2024-10-01 01:26:50 +0300

    added session manager

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,6 +1,7 @@
 const path = require("path").resolve(__dirname, "..");
 const signupService = require("../services/signupService");
 const customersService = require("../services/customers");
+const sessionManagerService = require("../services/sessionsManager");
 
 async function showSignupPage(req, res) {
   res.render("signup", {
@@ -12,20 +13,26 @@ async function showSignupPage(req, res) {
 async function registerUser(req, res) {
   console.log(req.body); // Check the incoming data
   const { firstName, lastName, email, gender, dob, password, confirmPassword } = req.body;
+
   try {
+    // Check if the email is already taken
     if (await customersService.getCustomerByEmail(email)) {
       return res.render("signup", {
         root: path,
         error: "Email already taken"
       });
     }
-    else {
-      await signupService.registerNewUser(firstName, lastName, email, gender, dob, password);
-      // Render to home page
-      res.redirect("/")
-    }
-  }
-  catch (error) {
+
+    // Register the new user in the database
+    const newUser = await signupService.registerNewUser(firstName, lastName, email, gender, dob, password);
+
+    // Set session details for the newly registered user
+    await sessionManagerService.setCustomerSession(req, newUser);
+
+    // Redirect to the home page after registration
+    res.redirect("/");
+
+  } catch (error) {
     console.error('Error saving user:', error);
     res.status(500).send('Server Error');
   }
---
+++ services/sessionsManager.js
@@ -0,0 +1,21 @@
+
+async function setCustomerSession(req, customerUser) {
+    try {
+        req.session.customer = {
+            id: customerUser.id,
+            firstName: customerUser.firstName,
+            lastName: customerUser.lastName,
+            email: customerUser.email,
+            gender: customerUser.gender,
+            birthDate: customerUser.birthDate,
+            isAdmin: customerUser.isAdmin
+        };
+        console.log('Session set for user:', req.session.customer);
+    } catch (error) {
+        console.error('Error setting session:', error);
+    }
+}
+
+module.exports = {
+    setCustomerSession
+};
