c0e569d1c1c8ed57614a27018ce6eba78d13c071
gabimatatov <gavriel.matatov@veeva.com>
2024-10-01 17:27:14 +0300

    added user icon if the user loged in

--- controllers/home.js
+++ controllers/home.js
@@ -2,11 +2,12 @@ const path = require("path").resolve(__dirname, "..");
 
 
 async function showCatalog(req, res) {
-    res.render("home", { root: path });
+    res.render("home", { 
+      root: path,
+      isAuthenticated: req.session.customer ? true : false
+    });
   }
 
 module.exports = {
-
     showCatalog
-
 }
