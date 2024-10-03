9b8ad7ae300ad69b9bf9969f9b85b87cf7f2a0d0
noamgloberman0 <noam.globerman@placer.ai>
2024-10-03 16:10:00 +0300

    fetching items from the database

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,5 @@
 const path = require("path").resolve(__dirname, "..");
+const productsService = require("../services/products");
 
 
 async function showAdminView(req, res) {
@@ -6,6 +7,16 @@ async function showAdminView(req, res) {
     // extract data from session
     var sessionCostumer = req.session.customer;
     var isAuthenticated = sessionCostumer ? true : false;
+
+    const items = await productsService.getAllProducts();
+    console.log(items);
+    const chunkSize = 4;
+    let result = [];
+
+    for (let i = 0; i < items.length; i += chunkSize) {
+        // Create a chunk of 4 items and push to result
+        result.push(items.slice(i, i + chunkSize));
+    }
 
     if (isAuthenticated) {
 
@@ -16,7 +27,8 @@ async function showAdminView(req, res) {
             res.render("admin", {
 
                 root: path,
-                isAuthenticated: isAuthenticated
+                isAuthenticated: isAuthenticated,
+                items: result
 
             });
 
