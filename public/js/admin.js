97a75a9d6966e1a4e9c8e8d59897c82f9ff18125
noamgloberman0 <noam.globerman@placer.ai>
2024-10-03 16:59:07 +0300

    deleting products work

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,8 +1,42 @@
-function editItem(itemName) {
-    window.alert("Editting " + itemName);
-}
-function deleteItem(itemName) {
+function editItem(item) {
 
-    window.alert("Deleting " + itemName);
+    item = JSON.parse(item);
+    window.alert("Editting " + item.cardName);
+
+}
+function deleteItem(item) {
+
+    item = JSON.parse(item);
+    if (confirm("Are you sure you want to delete  " + item.cardName + "?")) {
+        // Send a DELETE request to the backend
+        fetch(`/admin/${item.cardId}`, {
+            method: 'DELETE'
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+
+            if (data.status == 200) {
+
+                window.alert("successfully deleted " + item.cardName);
+
+            }
+            else {
+
+                window.alert("could not delete " + item.cardName + ": " + data.message);
+
+            }
+            
+            // Optionally, refresh the page or remove the item from the DOM
+            location.reload(); // Reloads the page to reflect changes
+        })
+        .catch(error => {
+            window.alert("Error deleting item: " + error.message);
+        });
+    }
 
 }
