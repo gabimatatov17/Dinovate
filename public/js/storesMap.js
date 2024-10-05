3e7709418e8554cd52ac594c858c45c7e9a26a05
gabimatatov <gavriel.matatov@veeva.com>
2024-10-05 12:56:02 +0300

    Added the use of JQuery Ajax in stores page

--- public/js/storesMap.js
+++ public/js/storesMap.js
@@ -1,6 +1,5 @@
-
 // Initialize the map function
-async function initMap() {
+function initMap(storesDetails) {
     const mapOptions = {
         center: { lat: 32.0853, lng: 34.7818 }, // Tel Aviv
         zoom: 12,
@@ -32,6 +31,7 @@ async function initMap() {
     });
 }
 
+
 // Function to create and display store details
 function addStoreDetails(store) {
     const storeList = document.getElementById('store-list');
@@ -48,7 +48,7 @@ function addStoreDetails(store) {
         <div class="store-details">
             <h4 style="font-weight: bold; font-size: 18px;">${store.storeName}</h4>
             <h4>${store.storeAdress}</h4>
-            <h4>Opening Houres: ${store.storeWorkingHours}</h4>
+            <h4>Opening Hours: ${store.storeWorkingHours}</h4>
             <h4>Opening Days: Sunday - Friday</h4>
         </div>
         <div class="store-phone">
@@ -61,3 +61,27 @@ function addStoreDetails(store) {
     // Append the store div to the store list
     storeList.appendChild(storeDiv);
 }
+
+
+// Function to fetch store details using jQuery AJAX
+function fetchStoreDetails() {
+    return $.ajax({
+        url: 'stores/api/getStoresDetails',
+        method: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            storesDetails = data;
+            // Call initMap once storesDetails are fetched
+            initMap(storesDetails);
+        },
+        error: function (err) {
+            console.error('Error fetching store details:', err);
+        }
+    });
+}
+
+
+// Fetch store details and then initialize the map
+$(document).ready(function () {
+    fetchStoreDetails();
+});
