835b34a5ff6aa60f8f677c8b8c038ef4ed44fa91
gabimatatov <gavriel.matatov@veeva.com>
2024-10-05 15:49:03 +0300

    Added style to login button in navbar.css

--- public/js/home.js
+++ public/js/home.js
@@ -2,11 +2,6 @@ const toggleButton = document.getElementById('toggle-button');
 const sidebar = document.getElementById('sidebar');
 const mainContent = document.querySelector('main');
 
-toggleButton.addEventListener('click', () => {
-    sidebar.classList.toggle('open');
-    mainContent.classList.toggle('shift');
-});
-
 function toggleProfileOptions() {
     const options = document.getElementById('profile-options');
     options.style.display = options.style.display === 'none' ? 'block' : 'none';
