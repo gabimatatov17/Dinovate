67b9bb15858f1ccb49152736ea4d11e070976b06
din.nehmadi <daniel.nehmadi@fiverr.com>
2024-10-06 20:45:05 +0300

    added home to pull all the images and when hovering the image name appeares

--- controllers/home.js
+++ controllers/home.js
@@ -1,11 +1,15 @@
 const path = require("path").resolve(__dirname, "..");
+const productsService = require("../services/products");
 
 
 async function showCatalog(req, res) {
 
+    const items = await productsService.getAllProducts();
+
     res.render("home", { 
-      root: path,
-      isAuthenticated: req.session.customer ? true : false
+        root: path,
+        isAuthenticated: req.session.customer ? true : false,
+        items: items
     });
   }
 
