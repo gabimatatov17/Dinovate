cd0225101467368adf7083fbcb8b3e5cfa161cd0
gabimatatov <gavriel.matatov@veeva.com>
2024-10-17 14:37:13 +0300

    changed titles & unused files

--- services/twitter.js
+++ services/twitter.js
@@ -1,20 +1,20 @@
 const { TwitterApi } = require('twitter-api-v2');
 
 const twitterClient = new TwitterApi({
-  appKey: process.env.TWITTER_API_KEY,            // Replace with your API key
-  appSecret: process.env.TWITTER_API_KEY_SECRET,  // Replace with your API secret key
-  accessToken: process.env.TWITTER_ACCESS_TOKEN,  // Replace with your access token
-  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET  // Replace with your access token secret
+    appKey: process.env.TWITTER_API_KEY,
+    appSecret: process.env.TWITTER_API_KEY_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 
-// Example function to post a tweet
+// Functions to post a tweet
 async function postProductToTwitter(product) {
     try {
         const tweetText = `
         New Card Alert: ${product.cardName}!\nPrice: $${product.price}.\nGo Check it out at Dinovate.com\n#dinovate`;
-        
-        const tweet = await twitterClient.v2.tweet(tweetText);  // Posting the tweet
+
+        const tweet = await twitterClient.v2.tweet(tweetText);
         return tweet;
 
     } catch (error) {
@@ -28,7 +28,7 @@ async function postStoreToTwitter(store) {
         const tweetText = `
         🌟 Exciting News! 🌟\nWe're thrilled to announce the opening of our new store: ${store.storeName}! 🎉\nCome visit us at ${store.storeAddress} 🛍️✨\nCan't wait to see you there!\nWorking Hours: ${store.workingHours}\n#NewStore #GrandOpening #Dinovate`;
 
-        const tweet = await twitterClient.v2.tweet(tweetText);  // Posting the tweet
+        const tweet = await twitterClient.v2.tweet(tweetText);
         return tweet;
 
     } catch (error) {
